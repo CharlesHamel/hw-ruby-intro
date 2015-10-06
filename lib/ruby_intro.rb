25dde833272519fcf5fe5539fc108587f227ce9b
Charles Hamel <chamel@simexperts.com>
2015-10-06 17:39:25 +0000

    Part 1,2 and 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,45 @@
 # When done, submit this entire file to the autograder.
 
-# Part 1
-
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.empty? ? 0 : arr.length == 1 ? arr.first : arr.sort.last(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? do |first, second| 
+    first + second == n
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.length > 0 && s !~ /^[AEIOU\d\W]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s.length > 0 && s.to_i(2) % 4 == 0 && s !~ /[^01]/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :price, :isbn
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price < 1
+    @price = price
+    @isbn = isbn
+  end
+
+  def price_as_string
+    "$%.2f" % price
+  end
 end
